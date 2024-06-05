002107826e5966afead0e4e6c623e2a418d65fd6
Borislav Petrovikj <borislav.petrovikj@inplayer.com>
2024-06-05 14:46:52 +0200

    fix: remove profiles

--- platforms/web/src/containers/RoutesContainer/RoutesContainer.tsx
+++ platforms/web/src/containers/RoutesContainer/RoutesContainer.tsx
@@ -5,20 +5,17 @@ import { useTranslation } from 'react-i18next';
 import { useProfileStore } from '@jwp/ott-common/src/stores/ProfileStore';
 import { useProfiles } from '@jwp/ott-hooks-react/src/useProfiles';
 import { useAccountStore } from '@jwp/ott-common/src/stores/AccountStore';
-import { PATH_USER_PROFILES } from '@jwp/ott-common/src/paths';
 import { shallow } from '@jwp/ott-common/src/utils/compare';
 import LoadingOverlay from '@jwp/ott-ui-react/src/components/LoadingOverlay/LoadingOverlay';
-import useQueryParam from '@jwp/ott-ui-react/src/hooks/useQueryParam';
 
 import useNotifications from '#src/hooks/useNotifications';
 
 const RoutesContainer = () => {
   const { i18n } = useTranslation();
   const location = useLocation();
-  const userModal = useQueryParam('u');
 
   const { profile, selectingProfileAvatar } = useProfileStore();
-  const { shouldManageProfiles } = useProfiles();
+  useProfiles();
 
   const userData = useAccountStore((s) => ({ loading: s.loading, user: s.user }), shallow);
 
@@ -39,11 +36,6 @@ const RoutesContainer = () => {
   // show a loading overlay when a profile is loading
   if (userData.user && selectingProfileAvatar !== null) {
     return <LoadingOverlay profileImageUrl={selectingProfileAvatar || profile?.avatar_url} />;
-  }
-
-  // the user must choose a profile first
-  if (shouldManageProfiles && !location.pathname.includes(PATH_USER_PROFILES) && !userModal) {
-    return <Navigate to={PATH_USER_PROFILES} />;
   }
 
   return <Outlet />;
