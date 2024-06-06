f023eca410da56ff8bc73fe032916a1683aa5118
Borislav Petrovikj <borislav.petrovikj@inplayer.com>
2024-06-06 11:28:48 +0200

    fix: remove code

--- platforms/web/src/containers/RoutesContainer/RoutesContainer.tsx
+++ platforms/web/src/containers/RoutesContainer/RoutesContainer.tsx
@@ -2,20 +2,14 @@ import React, { useEffect } from 'react';
 import { Navigate, useLocation } from 'react-router-dom';
 import { Outlet } from 'react-router';
 import { useTranslation } from 'react-i18next';
-import { useProfileStore } from '@jwp/ott-common/src/stores/ProfileStore';
-import { useProfiles } from '@jwp/ott-hooks-react/src/useProfiles';
 import { useAccountStore } from '@jwp/ott-common/src/stores/AccountStore';
 import { shallow } from '@jwp/ott-common/src/utils/compare';
-import LoadingOverlay from '@jwp/ott-ui-react/src/components/LoadingOverlay/LoadingOverlay';
 
 import useNotifications from '#src/hooks/useNotifications';
 
 const RoutesContainer = () => {
   const { i18n } = useTranslation();
   const location = useLocation();
-
-  const { profile, selectingProfileAvatar } = useProfileStore();
-  useProfiles();
 
   const userData = useAccountStore((s) => ({ loading: s.loading, user: s.user }), shallow);
 
@@ -31,11 +25,6 @@ const RoutesContainer = () => {
   // @todo should users be redirected away from a page after login?
   if (userData.user && !userData.loading && window.location.href.includes('#token')) {
     return <Navigate to={{ ...location, hash: '' }} replace />; // component instead of hook to prevent extra re-renders
-  }
-
-  // show a loading overlay when a profile is loading
-  if (userData.user && selectingProfileAvatar !== null) {
-    return <LoadingOverlay profileImageUrl={selectingProfileAvatar || profile?.avatar_url} />;
   }
 
   return <Outlet />;
